2a7ff00945a7b7bbe068e89918de49447c374f98
themailmangd <themailmangd@gmail.com>
2026-06-01 14:18:22 +1000

    signup now works!!!

--- landing_page/user_signup.php
+++ landing_page/user_signup.php
@@ -24,22 +24,22 @@
                                         <form action="signupaction.php" method="post">
                                             <h5>First Name</h5>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="text" id="firstName" placeholder="First Name" required />
+                                                <input class="form-control" type="text" name="first_name" id="firstName" placeholder="First Name" required />
                                                 <label for="inputLoginID">First Name</label>
                                             </div>
                                             <h5>Last Name</h5>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="text" id="lastName" placeholder="Last Name" required />
+                                                <input class="form-control" type="text" name="last_name" id="lastName" placeholder="Last Name" required />
                                                 <label for="inputPassword">Last Name</label>
                                             </div>
                                             <h5>Email</h5>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="text" id="email" placeholder="Email" required />
+                                                <input class="form-control" type="text" name="email" id="email" placeholder="Email" required />
                                                 <label for="inputPassword">Email</label>
                                             </div>
                                             <h5>Password | 6-12 characters* </h5>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" type="text" name="text" id="password" placeholder="Password" required />
+                                                <input class="form-control" type="text" name="password" id="password" placeholder="Password" required />
                                                 <label for="inputPassword">Password</label>
                                             </div>
                                             
